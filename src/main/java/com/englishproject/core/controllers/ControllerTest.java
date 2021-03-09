fbc7a1eb3f6217c069ae2b719465a5e80805bfb6
KelinNik <kelin666@mail.ru>
2021-03-09 16:47:51 +0300

    init commit

---
+++ src/main/java/com/englishproject/core/controllers/ControllerTest.java
@@ -0,0 +1,14 @@
+package com.englishproject.core.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerTest {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello !!!";
+    }
+}
+
